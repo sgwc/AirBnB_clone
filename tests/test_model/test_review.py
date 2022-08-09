ca3fe8d55bc0b1e4737c1d5600b2d18a0158426a
sgwc <45393676+sgwc@users.noreply.github.com>
2022-08-09 08:03:35 +0300

    Create test_review.py

---
+++ tests/test_model/test_review.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Unittest for BaseModel class
+"""
+import unittest
+from models.review import Review
+import pep8
+from models.base_model import BaseModel
+
+
+class TestAmenity(unittest.TestCase):
+    """ Test cases for Amenity class """
+
+    def test_pep8_conformance_model(self):
+        """
+        Test that we conform to PEP8.
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/review.py'])
+        self.assertEqual(result.total_errors, 0, "Fix pep8")
+
+    def test_docstring(self):
+        """
+        Testing docstring
+        """
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_instance_BaseModel(self):
+        """ Tests inheritance """
+        amenity = Review()
+        self.assertTrue(isinstance(amenity, BaseModel))
+
+    def test_instaciacion(self):
+        """ Tests correct instatiation of the class """
+        am = Review()
+        am.name = "Charlie"
+        self.assertIn("name", am.to_dict())
+
+    def test_to_dict(self):
+        """ Tests that the function retrieves a dictionary """
+        base = Review()
+        ret_dict = base.to_dict()
+        self.assertTrue(isinstance(ret_dict, dict))
+
+    def test_str(self):
+        """ Tests the str repr. of an object """
+        base = Review()
+        base_str = base.__str__()
+        self.assertTrue(isinstance(base_str, str))
+
+    def test_save(self):
+        """ Tests the save method """
+        base = Review()
+        time1 = base.updated_at
+        base.save()
+        time2 = base.updated_at
+        self.assertNotEqual(time1, time2)
